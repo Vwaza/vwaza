febcedd0820a3a7933844f033e0c8fb8adde5f36
Joel Fickson <Joelngozo@gmail.com>
2023-11-12 14:08:55 +0100

    chore: add styles and basic info

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import '../app.css';
+</script>
+
+<div class="flex justify-center md:w-1/3 w-full flex-col items-center mx-auto">
+	<h4 class="text-2xl text-primary">Vwaza Is Coming Back</h4>
+	<p class="text-lg text-secondary">Helping African Creators Showcase Their Talent Through Music</p>
+	<p>Get In Touch :: <a href="mailto:info@vwaza.com">info@vwaza.com</a></p>
+</div>
